e448176fda97875a926dd60e040a3a7f10705acb
mfaisal-lab <m.faisal@students.amikom.ac.id>
2026-04-09 14:59:56 +0700

    update2

--- calculator.cs
+++ calculator.cs
@@ -2,5 +2,21 @@ class Calculator
 {
 	static void main(string[]args)
 	{
+		int a = 10;
+		int b = 6;
+		
+		Console.WriteLine("Hasil Penambahan: {0} + {1} = {2}", a, b, Penambahan(a, b));
+		Console.WriteLine("Hasil Pengurangan: {0} - {1} = {2}", a, b, Pengurangan(a, b));
+		
+		Console.WriteLine("\nTekan sembabarangan key untuk keluar");
+		Console.WriteLine();
+	}
+	static int Penambahan(int a, int b)
+	{
+		return a + b;
+	}
+	static int Pengurangan(int a, int b)
+	{
+		return a - b;
 	}
 }
